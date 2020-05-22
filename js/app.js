fdd9b1aba36d946500cbc34a83b510026c98ed46
laugeeme <registro.laura@gmail.com>
2020-05-22 14:23:45 +0200

    read value from selected model

--- js/app.js
+++ js/app.js
@@ -1,7 +1,35 @@
 /* eslint-disable no-console */
 'use strict';
 
-/* function Insurance() {} */
+//insurance constructor
+function Insurance(model, year, type) {
+  this.model = model;
+  this.year = year;
+  this.type = type;
+}
+
+//interfaz
+function Interface() {}
+
+
+
+
+//listneners
+const form = document.getElementById('cotizar-seguro');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //read selected model
+  const model = document.getElementById('marca');
+  const modelSelected = model.options[model.selectedIndex].value;
+
+  console.log(modelSelected);
+
+});
+
+
+
+
 
 //add options in select input
 const max = new Date().getFullYear(),
@@ -11,6 +39,6 @@ const selectYears = document.getElementById('anio');
 for (let i = max; i > min; i--) {
   let option = document.createElement('option');
   option.value = i;
-  option.innerHTML =i;
+  option.innerHTML = i;
   selectYears.appendChild(option);
 }
